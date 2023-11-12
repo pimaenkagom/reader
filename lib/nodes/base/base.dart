e13c0b1f7927310e216f00813fec54cfc77824ce
Miena Basta Badres <dev@mbadres.de>
2023-11-12 01:11:19 +0100

    Start implementing the base node.

--- lib/nodes/base/base.dart
+++ lib/nodes/base/base.dart
@@ -2,8 +2,10 @@ import 'package:uuid/uuid.dart';
 
 class Base {
 
-  final Uuid id;
+  final String id = const Uuid().v4();
   final String title;
+  final List<Base> children;
+  final List<String> tags;
 
-  const Base(this.title) : id = const Uuid();
+  Base({required this.title, required this.children, required this.tags,});
 }
