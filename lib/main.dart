05835f9d8f567f6af449142b647dfa56f25f0324
Miena Basta Badres <dev@mbadres.de>
2024-01-09 00:46:23 +0100

    Remove sample code.

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,11 @@ import 'package:reader/nodes/line/line.dart';
 import 'package:reader/nodes/clause/clause.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -34,63 +34,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       darkTheme: ThemeData.dark(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('You have pushed the button this many times:'),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              // LibraryWidget(model: Library('Library - Bücherei')),
-              // CollectionWidget(model: Collection('Collection - Reihe')),
-              // BookWidget(model: Book('Book - Buch')),
-              // PartWidget(model: Part('Part - Teil')),
-              // ChapterWidget(model: Chapter('Chapter - Kapitel')),
-              // SectionWidget(model: Section('Section - Abschnitt')),
-              // ParagraphWidget(model: Paragraph('Paragraph - Absatz')),
-              // LineWidget(model: Line('Line - Satz')),
-              // ClauseWidget(model: Clause('Clause - Teilsatz')),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: Container(),
     );
   }
 }
